58ff60d9c3db2adc057951ada81c0733d79e131a
avatGit <dankfd99@gmail.com>
2026-08-28 00:58:34 +0000

    Dashboard Screen

--- lib/presentation/screens/dashboard/dashboard_screen.dart
+++ lib/presentation/screens/dashboard/dashboard_screen.dart
@@ -1,14 +1,35 @@
+import 'package:culinary_craft/data/repositories/recipe_repository.dart';
 import 'package:flutter/cupertino.dart';
 
+import '../../../data/models/recipe.dart';
+
 class DashboardScreen extends StatelessWidget {
-  const DashboardScreen({super.key});
+  final RecipesRepository repository;
+
+  const DashboardScreen({super.key, required this.repository});
 
   @override
   Widget build(BuildContext context) {
+    final recipes = repository.getAllRecipe();
+
+    // Nombre total de recettes
+    final totalRecipes = recipes.length;
+
+    // `.toSet()` supprime automatiquement les doublons.
+    final categoriesCount = recipes.map((r) => r.category).toSet().length;
+
+    final double avgDuration = recipes.isEmpty
+        ? 0
+        : recipes.fold(0, (sum, recipe) => sum + recipe.duration) /
+              totalRecipes;
+
+    final easyRecipesCount = recipes
+        .where((r) => r.difficulty.toLowerCase() == 'Facile')
+        .length;
+
+    // Recuperer la derniere recette
+    final Recipe? latestRecipe = recipes.isNotEmpty ? recipes.last : null;
     // TODO: implement build
     throw UnimplementedError();
   }
-
-  // @override
-  // State<DashboardScreen> createState() => _DashboardScreenState();
 }
